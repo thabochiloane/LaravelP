e19ae9b8e9f7d37a80a87e3e83f75d31d95d94ba
Thabo Chiloane <thabo.chiloane@hotmail.com>
2021-01-16 17:28:39 +0200

    Modifiying the home page and navigation bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading"><strong>Welcome To My Laravel Assessment</strong></div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Welcome back, this is the web application where user can subscribe more users into the system. Click on Login to login into the system or click on register to register a new Admin user.
                 </div>
             </div>
         </div>
