e3d56a3e3fb9549821ac7dccbded4f91cd2fa046
Thabo Chiloane <thabo.chiloane@hotmail.com>
2021-01-16 20:54:27 +0200

    creating the connection between controller and the database fields

--- app/UserData.php
+++ app/UserData.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserData extends Model
 {
-    //
+    protected $table = 'user_data';
+    protected $fillable = [
+        'name', 'surname', 'idNumber', 'mobileNumber', 'email', 'dateOfBirth', 'language', 'interests'
+    ];
+
 }
