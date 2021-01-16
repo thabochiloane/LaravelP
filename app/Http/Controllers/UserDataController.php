2e98327cb515b620b9dd612da9159505ad84d3ea
Thabo Chiloane <thabo.chiloane@hotmail.com>
2021-01-16 20:18:35 +0200

    creating userdatacontroller control with a list of resources

--- app/Http/Controllers/UserDataController.php
+++ app/Http/Controllers/UserDataController.php
@@ -9,6 +9,17 @@ use App\Http\Requests;
 
 class UserDataController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $userData = UserData::latest()->paginate(5);
+  
+        return view('userData.index',compact('userData'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
 
 }
